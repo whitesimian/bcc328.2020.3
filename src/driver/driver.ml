241341390a59eabcf6325b30ab8eaafd69f91a4a
José Romildo Malaquias <malaquias@gmail.com>
2020-09-08 09:29:07 -0300

    Semantic: adaptations to Driver

--- src/driver/driver.ml
+++ src/driver/driver.ml
@@ -30,7 +30,15 @@ let main () =
     print_endline "============================================================";
     let tree = Absyntree.flat_nodes (Absyntree.tree_of_lexp ast) in
     let boxtree = Tree.box_of_tree tree in
-    print_endline (Box.string_of_box boxtree)
+    print_endline (Box.string_of_box boxtree);
+    print_endline "Semantic analysis:";
+    print_endline "============================================================";
+    Semantic.semantic ast;
+    match ast with
+    | (_, (_, { contents = Some ty })) ->
+       print_endline (Type.show_ty ty)
+    | _ ->
+       Error.fatal "cannot determine type of program"
   with
   | Error.Error (loc, msg) ->
      Format.printf "%a error: %s\n" Location.pp_location loc msg;
