ecef4a99792dd2f03addd4266644a88b6f426613
artur <arturfernandescunha@hotmail.com>
2020-09-08 21:59:07 -0300

    Semantic Analysis - <int> type representation

--- src/lib/type.ml
+++ src/lib/type.ml
@@ -4,6 +4,7 @@
 type ty =
   | VOID
   | BOOL
+  | INT
 [@@deriving show]
 
 (* type compatibility *)
@@ -11,9 +12,12 @@ let coerceable a b =
   match a, b with
   | VOID , VOID -> true
   | BOOL , BOOL -> true
+  | INT  , INT  -> true
   | _           -> false
 
 (* general tree from internal type representation *)
 let tree_of = function
   | VOID -> Tree.mkt "VOID" []
   | BOOL -> Tree.mkt "BOOL" []
+  | INT  -> Tree.mkt "INT"  []
+
