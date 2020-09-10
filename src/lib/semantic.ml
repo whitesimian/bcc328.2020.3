e4e7dbdaf5a141b136a49575d93ad7ec22aa81e9
José Romildo Malaquias <malaquias@gmail.com>
2020-09-09 21:10:48 -0300

    Semantic: check let expression

--- src/lib/semantic.ml
+++ src/lib/semantic.ml
@@ -16,10 +16,20 @@ let set reference value =
 
 let rec check_exp env (pos, (exp, tref)) =
   match exp with
-  | A.BoolExp _   -> set tref T.BOOL
-  | A.IntExp  _   -> set tref T.INT
-  | A.RealExp _   -> set tref T.REAL
+  | A.BoolExp _ -> set tref T.BOOL
+  | A.IntExp  _ -> set tref T.INT
+  | A.RealExp _ -> set tref T.REAL
   | A.StringExp _ -> set tref T.STRING
+  | A.LetExp (decs, body) -> check_exp_let env pos tref decs body
+  | _ -> Error.fatal "unimplemented"
+
+and check_exp_let env pos tref decs body =
+  let env' = List.fold_left check_dec env decs in
+  let tbody = check_exp env' body in
+  set tref tbody
+
+and check_dec env (pos, dec) =
+  match dec with
   | _ -> Error.fatal "unimplemented"
 
 let semantic program =
